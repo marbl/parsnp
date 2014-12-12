e0f50d7f3dc5e410c9b5848b3d31a6277a10579f
Todd J Treangen <treangen@gmail.com>
2014-12-12 14:43:33 -0500

    Update build_parsnp_linux.sh

--- build_parsnp_linux.sh
+++ build_parsnp_linux.sh
@@ -5,5 +5,5 @@ make install
 cd ..
 ./autogen.sh
 export LD_LIBRARY_PATH="/home/travis/build/marbl/parsnp/muscle/lib":$LD_LIBRARY_PATH
-./configure --with-libmuscle=/home/travis/build/marbl/parsnp/muscle/lib 
+./configure --with-libmuscle=/home/travis/build/marbl/parsnp/muscle/ 
 make install
