93c360209f56c1d19aeb80c5b32f94c3e1efdeef
Bryce Kille <brycekille@gmail.com>
2020-06-03 16:19:16 -0500

    fix syntax

--- build_parsnp.sh
+++ build_parsnp.sh
@@ -1,10 +1,9 @@
 cd muscle
 ./autogen.sh
-./configure --prefix=$PWD CXXFLAGS='-fopenmp'
+./configure --prefix=$PWD CXXFLAGS='-fopenmp=libgomp'
 make install
 cd ..
 ./autogen.sh
 ./configure
-echo "Fix MUSCLE-3.7 linker" 
 make LDADD=-lMUSCLE-3.7
 make install
